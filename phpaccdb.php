cf6f5988829452efccd5075e02555b694ce2d291
adhipramudya01 <128989919+adhipramudya01@users.noreply.github.com>
2023-03-26 23:18:13 +0700

    Update phpaccdb.php

--- phpaccdb.php
+++ phpaccdb.php
@@ -1,11 +1,16 @@
 <?php 
-$db_username = ''; //username $db_password = ''; //password 
+$db_username = ''; //username 
+$db_password = ''; //password 
 
-//path to database file $database_path = $_SERVER["DOCUMENT_ROOT"] . "\access\accessdatabase.accdb"; 
+//path to database file 
+$database_path = $_SERVER["DOCUMENT_ROOT"] . "\access\accessdatabase.accdb"; 
 
-//check file exist before we proceed if (!file_exists($database_path)) { die("Access database file not found !"); }
+//check file exist before we proceed 
+if (!file_exists($database_path)) 
+{ die("Access database file not found !"); }
 
- //create a new PDO object $database = new PDO("odbc:DRIVER={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=$database_path; Uid=$db_username; Pwd=$db_password;"); 
+ //create a new PDO object 
+$database = new PDO("odbc:DRIVER={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=$database_path; Uid=$db_username; Pwd=$db_password;"); 
 
 $sql = "SELECT * FROM userdetails"; $result = $database->query($sql); 
 
